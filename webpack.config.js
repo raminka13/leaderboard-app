1d09efca0d57d4e9d1b7ee5cb08ea9fc6ff5bf26
Raminka <raminka13@hotmail.com>
2022-07-19 12:46:26 -0500

    Add html structure, basic styles, basic functions

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
