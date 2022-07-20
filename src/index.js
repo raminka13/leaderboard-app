c616f3b6e73938013a357ccb3a8c29642df09256
Raminka <raminka13@hotmail.com>
2022-07-20 11:41:20 -0500

    Required changes

--- src/index.js
+++ src/index.js
@@ -15,23 +15,11 @@ document.querySelector('#form-section').addEventListener('submit', (e) => {
   if (name === '' || points === '') {
     UI.showAlert('Please fill in all fields', 'danger', 1500);
   } else {
-    // fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     name: 'Duel of Fates',
-    //   }),
-    // })
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data.result));
     // Start a new Score
     API.sendData(name, points);
-    UI.showAlert('Score Added', 'success', 1500);
+    UI.showAlert('Score Added, Please Refresh', 'success', 1500);
     // Clear fields
     UI.clearFields();
-    setTimeout(UI.deleteList, 2000);
   }
 });
 
